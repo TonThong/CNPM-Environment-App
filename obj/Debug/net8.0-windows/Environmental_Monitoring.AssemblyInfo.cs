ae2f129fa5158a5cfd657c15d7d032a4a82dec75
TonThong <thongton2310.eht.edu@gmail.com>
2025-10-15 09:43:48 +0700

    Update ContractPlan

--- obj/Debug/net8.0-windows/Environmental_Monitoring.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Environmental_Monitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe84d9c316576c5e1b28ead54b01423d7450784")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4efa045c6ba585dbd4d7a9b2c3e4d5ccfd2e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
