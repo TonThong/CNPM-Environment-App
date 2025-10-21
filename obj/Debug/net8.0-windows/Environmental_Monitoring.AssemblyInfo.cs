6d9e5a048f79aa7bc4a1375e69b86b3602535cfd
TonThong <thongton2310.eht.edu@gmail.com>
2025-10-21 19:37:09 +0700

    Git Test

--- obj/Debug/net8.0-windows/Environmental_Monitoring.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Environmental_Monitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3c5aa7ec81e4f367cac2b640fe675b863cd80e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5da211aba447034d8af85ac1676f74a3088443")]
 [assembly: System.Reflection.AssemblyProductAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
