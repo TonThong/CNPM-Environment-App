f0ad29f6d55df296d742e908546b5937f71aa624
Chi Tuong <chituongluongvan@gmail.com>
2025-11-04 11:09:56 +0700

    update

--- obj/Debug/net8.0-windows/Environmental_Monitoring.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Environmental_Monitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffeffe62a9df10c7825775e0e3b8a9bcee49dc61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4582475530300d69634786b475a63a8d5bd69a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Environmental_Monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
